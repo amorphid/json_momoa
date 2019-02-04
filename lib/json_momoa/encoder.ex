3c1d4f378e29fa95b7430d004a4afaef98a79da1
Michael Pope <mpope.cr@gmail.com>
2019-02-03 21:40:58 -0800

    Add encoding for strings

--- lib/json_momoa/encoder.ex
+++ lib/json_momoa/encoder.ex
@@ -5,6 +5,10 @@ end
 alias JSONMomoa.Encoder
 
 defimpl Encoder, for: Atom do
+  #######
+  # API #
+  #######
+
   def to_json(true) do
     "true"
   end
@@ -15,5 +19,71 @@ defimpl Encoder, for: Atom do
 
   def to_json(nil) do
     "null"
+  end
+end
+
+defimpl Encoder, for: BitString do
+  #######
+  # API #
+  #######
+
+  def to_json(data) do
+    in_string(data, [?\"])
+  end
+
+  ###########
+  # Private #
+  ###########
+
+  defp in_string("", acc) do
+    [?\" | acc]
+    |> Enum.reverse()
+    |> to_string()
+  end
+
+  defp in_string(<<head::utf8(), tail::bits()>>, acc) when head in 0x20..0x21 or head in 0x23..0x2e or head in 0x30..0x5B or head in 0x5D..0x10FFFF do
+    in_string(tail, [head | acc])
+  end
+
+  defp in_string("\"" <> data, acc) do
+    in_string(data, [?", ?\\ | acc ])
+  end
+
+  defp in_string("\\" <> data, acc) do
+    in_string(data, [?\\, ?\\ | acc ])
+  end
+
+  defp in_string("\/" <> data, acc) do
+    in_string(data, [?/, ?\\ | acc ])
+  end
+
+  defp in_string("\b" <> data, acc) do
+    in_string(data, [?b, ?\\ | acc ])
+  end
+
+  defp in_string("\f" <> data, acc) do
+    in_string(data, [?f, ?\\ | acc ])
+  end
+
+  defp in_string("\n" <> data, acc) do
+    in_string(data, [?n, ?\\ | acc ])
+  end
+
+  defp in_string("\r" <> data, acc) do
+    in_string(data, [?r, ?\\ | acc ])
+  end
+
+  defp in_string("\t" <> data, acc) do
+    in_string(data, [?t, ?\\ | acc ])
+  end
+
+  defp in_string(<<head::utf8(), tail::bits()>>, acc) do
+    case Integer.to_charlist(head, 16) do
+      [num1] -> 
+        in_string(tail, [num1, ?0, ?0, ?0, ?u, ?\\ | acc ])
+
+      [num1, num2] -> 
+        in_string(tail, [num2, num1, ?0, ?0, ?u, ?\\ | acc ])
+    end
   end
 end
--- test/json_momoa/encoder_test.exs
+++ test/json_momoa/encoder_test.exs
@@ -20,4 +20,102 @@ defmodule JSONMomoa.EncoderTest do
       assert @subject.to_json(nil) === "null"
     end
   end
+
+  describe "encoding an empty string" do
+    test "returns the expected string" do
+      assert @subject.to_json("") === "\"\""
+    end
+  end
+
+  describe "encoding the string \"abc\"" do
+    test "returns the expected string" do
+      assert @subject.to_json("abc") === "\"abc\""
+    end
+  end
+
+  describe "encoding the \" character" do
+    test "returns the expected string" do
+      assert @subject.to_json("\"") === "\"\\\"\""
+    end
+  end
+
+  describe "encoding the \\ character" do
+    test "returns the expected string" do
+      assert @subject.to_json("\\") === "\"\\\\\""
+    end
+  end
+
+  describe "encoding the / character" do
+    test "returns the expected string" do
+      assert @subject.to_json("/") === "\"\\/\""
+    end
+  end
+
+  describe "encoding the \b character" do
+    test "returns the expected string" do
+      assert @subject.to_json("\b") === "\"\\b\""
+    end
+  end
+
+  describe "encoding the \f character" do
+    test "returns the expected string" do
+      assert @subject.to_json("\f") === "\"\\f\""
+    end
+  end
+
+  describe "encoding the \n character" do
+    test "returns the expected string" do
+      assert @subject.to_json("\n") === "\"\\n\""
+    end
+  end
+
+  describe "encoding the \r character" do
+    test "returns the expected string" do
+      assert @subject.to_json("\r") === "\"\\r\""
+    end
+  end
+
+  describe "encoding the \t character" do
+    test "returns the expected string" do
+      assert @subject.to_json("\t") === "\"\\t\""
+    end
+  end
+
+  describe "encoding various control characters" do
+    test "returns the expected string" do
+      assert @subject.to_json(<<0>>) === "\"\\u0000\""
+      assert @subject.to_json(<<1>>) === "\"\\u0001\""
+      assert @subject.to_json(<<2>>) === "\"\\u0002\""
+      assert @subject.to_json(<<3>>) === "\"\\u0003\""
+      assert @subject.to_json(<<4>>) === "\"\\u0004\""
+      assert @subject.to_json(<<5>>) === "\"\\u0005\""
+      assert @subject.to_json(<<6>>) === "\"\\u0006\""
+      assert @subject.to_json(<<7>>) === "\"\\u0007\""
+      assert @subject.to_json(<<11>>) === "\"\\u000B\""
+      assert @subject.to_json(<<14>>) === "\"\\u000E\""
+      assert @subject.to_json(<<15>>) === "\"\\u000F\""
+      assert @subject.to_json(<<16>>) === "\"\\u0010\""
+      assert @subject.to_json(<<17>>) === "\"\\u0011\""
+      assert @subject.to_json(<<18>>) === "\"\\u0012\""
+      assert @subject.to_json(<<19>>) === "\"\\u0013\""
+      assert @subject.to_json(<<20>>) === "\"\\u0014\""
+      assert @subject.to_json(<<21>>) === "\"\\u0015\""
+      assert @subject.to_json(<<22>>) === "\"\\u0016\""
+      assert @subject.to_json(<<23>>) === "\"\\u0017\""
+      assert @subject.to_json(<<24>>) === "\"\\u0018\""
+      assert @subject.to_json(<<25>>) === "\"\\u0019\""
+      assert @subject.to_json(<<26>>) === "\"\\u001A\""
+      assert @subject.to_json(<<27>>) === "\"\\u001B\""
+      assert @subject.to_json(<<28>>) === "\"\\u001C\""
+      assert @subject.to_json(<<29>>) === "\"\\u001D\""
+      assert @subject.to_json(<<30>>) === "\"\\u001E\""
+      assert @subject.to_json(<<31>>) === "\"\\u001F\""
+    end
+  end
+
+  describe "encoding string \"hełło\"" do
+    test "returns expected string" do
+      assert @subject.to_json("hełło") === "\"hełło\""
+    end
+  end
 end
