aeeb57f7b9ac0dc9c7ebd29d00f0b8e298846a44
Michael Pope <mpope.cr@gmail.com>
2019-02-04 19:53:22 -0800

    Add metadata for Hex publishing

--- mix.exs
+++ mix.exs
@@ -1,31 +1,51 @@
 defmodule JSONMomoa.MixProject do
   use Mix.Project
 
-  def project do
-    [
-      app: :json_momoa,
-      version: "0.1.0",
-      elixir: "~> 1.8",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
-    ]
-  end
+  #######
+  # API #
+  #######
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application() do
     [
       extra_applications: [:logger],
       mod: {JSONMomoa.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  def project() do
+    [
+      app: :json_momoa,
+      deps: deps(),
+      description: description(),
+      # feel free to submit pull request w/ lower Elixir version 
+      elixir: "~> 1.8",
+      package: package(),
+      start_permanent: Mix.env() == :prod,
+      version: "0.1.0"
+    ]
+  end
+
+  ###########
+  # Private #
+  ###########
+
+  defp deps() do
     [
       # static analysis
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
       # document generation
       {:ex_doc, ">= 0.0.0", only: [:dev]}
+    ]
+  end
+
+  defp description() do
+    "A streaming JSON parser/encoder written in pure Elixir."
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/amorphid/json_momoa"}
     ]
   end
 end
