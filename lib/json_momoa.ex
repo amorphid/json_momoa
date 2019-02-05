1fd4d4715be594ca643df20af3ce26951c485a34
Michael Pope <mpope.cr@gmail.com>
2019-02-04 20:18:55 -0800

    Fix Elixir code formatting in documentation

--- lib/json_momoa.ex
+++ lib/json_momoa.ex
@@ -12,13 +12,13 @@ defmodule JSONMomoa do
 
   Example:
 
-      iex> {element0, data0} = JSONMomoa.parse("truefalse{}\"aw yiss\"")
-      {true, "false{}\"aw yiss\""}
-      iex>  {element1, data1} = JSONMomoa.parse("false{}\"aw yiss\"")
-      {false, "{}\"aw yiss\""}
-      iex>  {element2, data2} = JSONMomoa.parse("{}\"aw yiss\"")
-      {%{}, "\"aw yiss\""}
-      iex>  {element3, data3} = JSONMomoa.parse("\"aw yiss\"")
+      iex> {element0, data0} = JSONMomoa.parse("truefalse{}\\\"aw yiss\\\"")
+      {true, "false{}\\\"aw yiss\\\""}
+      iex>  {element1, data1} = JSONMomoa.parse("false{}\\\"aw yiss\\\"")
+      {false, "{}\\\"aw yiss\\\""}
+      iex>  {element2, data2} = JSONMomoa.parse("{}\\\"aw yiss\\\"")
+      {%{}, "\\\"aw yiss\\\""}
+      iex>  {element3, data3} = JSONMomoa.parse("\\\"aw yiss\\\"")
       {"aw yiss", ""}
   """
   defdelegate parse(data), to: JSONMomoa.Parser
@@ -29,7 +29,7 @@ defmodule JSONMomoa do
   Example:
 
       iex> JSONMomoa.to_json([%{hello: "world"}, 1, 2, nil])
-      "[{\"hello\":\"world\"},1,2,null]"
+      "[{\\\"hello\\\":\\\"world\\\"},1,2,null]"
   """
   defdelegate to_json(data), to: JSONMomoa.Encoder
 end
